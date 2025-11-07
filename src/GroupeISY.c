22d845d4880c3cb0987772023e5d501f9f8f1153
bryan <br.pignerol@gmail.com>
2025-11-07 14:56:48 +0100

    Header des fichiers

---
+++ src/GroupeISY.c
@@ -0,0 +1,21 @@
+/*============================================================================*
+ * ISY MESSAGERIE - GroupeISY.c
+ *============================================================================*
+ * Auteur       : Votre Nom
+ * Date         : 07/11/2025
+ * Version      : 1.0
+ *----------------------------------------------------------------------------*
+ * Description  : Gestion des discussions au sein d'un groupe de messagerie
+ *                Traitement des messages et synchronisation via SHM
+ *============================================================================*/
+
+ // INCLUDES SYSTEME
+#include <stdio.h>
+
+// INCLUDES HEADER
+#include "GroupeISY.h"
+
+
+int main(int argc, char *argv[]) {
+
+}
